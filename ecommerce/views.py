2cbbda58b9bb220394c193dbfe14cef41104adf5
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2022-07-20 13:17:50 +0100

    Added forms.py file for contact form

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import ContactForm
 
 def home_page(request):
     context = {
@@ -16,10 +17,14 @@ def about_page(request):
     return render(request, "home_page.html", context)
 
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
     context = {
     "title":"Contact Page",
-    "content":"Welcome to Contactpage"
+    "content":"Welcome to Contactpage",
+    "form": contact_form
     }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
     if request.method == "POST":
         print(request.POST)
         print(request.POST.get('fullname'))
