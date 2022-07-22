0098dbb6768e8f5fe69d2bf4e6a6b83d96ff5080
Nicolae Caldarasan <nicolaecaldarasan1983@gmail.com>
2022-07-22 13:27:34 +0100

    User Register Form

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,12 +1,13 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from .forms import ContactForm, LoginForm
+from .forms import ContactForm, LoginForm, RegisterForm
 
 def home_page(request):
     context = {
     "title":"Home Page",
-    "content":"Welcome to homepage"
+    "content":"Welcome to homepage",
+    "premium_content": "Yeahhhhhhhh"
     }
     return render(request, "home_page.html", context)
 
@@ -46,17 +47,26 @@ def login_page(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             # context['form'] = LoginForm()
-            return redirect("/login")
+            return redirect("/")
         else:
             print("ERROR")
 
     return render(request, "login.html", context)
 
+User = get_user_model()
 def register_page(request):
-    form = LoginForm(request.POST or None)
+    form = RegisterForm(request.POST or None)
+    context = {
+        "form": form
+        }
     if form.is_valid():
         print(form.cleaned_data)
-    return render(request, "register.html", {})
+        username = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        new_user = User.objects.create_user(username, email, password)
+        print(new_user)
+    return render(request, "register.html", context)
 
 
 def home_page_old(request):
